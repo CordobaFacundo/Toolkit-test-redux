2bbeca9691c71852bee3ae01be5a55e42674948d
CordobaFacundo <facundo.cordoba2012@gmail.com>
2025-02-06 12:37:29 -0300

    RTK query use data

--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { buildErrorMessage } from "vite";
+// import { buildErrorMessage } from "vite";
 
 export const todosApi = createApi ({
 
